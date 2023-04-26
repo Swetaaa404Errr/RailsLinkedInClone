2da3312ef0d05a59458497076d015778ca07d7bf
Sweta <swetadas1099@gmail.com>
2023-04-27 00:48:56 +0530

    changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,30 +10,36 @@
 
 require 'faker'
 
-admins = User.create(email: 'admin333@gmail.com', username: 'admin333', password: 'add223',
-                     password_confirmation: 'add223', admin: true)
+# in your seeds.rb file
 
-admins = User.create(email: 'iamadmin@gmail.com', username: 'iamadmin', password: 'add2',
-                     password_confirmation: 'add2', admin: true)
+# generate 20 dummy users with ids ranging from 14 to 35
+users = []
+(1..30).each do |i|
+  user = User.create!(
+    email: Faker::Internet.email,
+    username: Faker::Internet.username,
+    password: "password", # or use Faker::Internet.password to generate a random password
+    password_confirmation: "password",
+    admin: false,
+    id: i
+  )
+   users << user
+end
+(31..33).each do |i|
+  User.create!(
+    email: Faker::Internet.email,
+    username: Faker::Internet.username,
+    password: "password", # or use Faker::Internet.password to generate a random password
+    password_confirmation: "password",
+    admin: true,
+    id: i
+  )
+end
 
-users = User.create(email: 'trieluser@gmail.com', username: 'user22', password: 'password',
-                    password_confirmation: 'password', admin: false)
-
-users = User.create(email: 'user@gmail.com', username: 'user', password: 'user1',
-                    password_confirmation: 'user1', admin: false)
-
-
-
-users = User.create(email: 'sritam9@gmail.com', username: 'sritam9', password: 'srisri',
-                    password_confirmation: 'srisri', admin: false)
-
-user_ids = [4, 5]
-
-UserAccount.where(user_id: user_ids).destroy_all
 
 user_data = [
   {
-    user_id: 4,
+    user_id: 1,
     username: 'user12',
     gmail: 'user@gmail.com',
     link: nil,
@@ -50,6 +56,57 @@ user_data = [
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
   },
   {
+    user_id: 2,
+    username: 'user2',
+    gmail: 'sritamd9@gmail.com',
+    link: nil,
+    qualification: 'BE',
+    experience: '5-10',
+    organisation: 'XYZ Company',
+    skill: 'React,ajile',
+    notification: true,
+    cv: nil,
+    picture: nil,
+    certificate: nil,
+    job: 'Cook,WebEngineer,Engineer,Receiptionist,Caretaker,Developer',
+    cvdownload: 'Connections',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  {
+    user_id: 3,
+    username: 'user3',
+    gmail: 'sritamd9@gmail.com',
+    link: nil,
+    qualification: 'BE',
+    experience: '5-10',
+    organisation: 'XYZ Company',
+    skill: 'editing,devops',
+    notification: true,
+    cv: nil,
+    picture: nil,
+    certificate: nil,
+    job: 'Cook,Engineer',
+    cvdownload: 'Connections',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  {
+    user_id: 4,
+    username: 'user4',
+    gmail: 'sritamd9@gmail.com',
+    link: nil,
+    qualification: 'BE',
+    experience: '5-10',
+    organisation: 'XYZ Company',
+    skill: 'python,java',
+    notification: true,
+    cv: nil,
+    picture: nil,
+    certificate: nil,
+    job: 'Cook,Receiptionist,Caretaker,Developer',
+    cvdownload: 'Connections',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  {
     user_id: 5,
     username: 'sritam9',
     gmail: 'sritamd9@gmail.com',
@@ -57,7 +114,24 @@ user_data = [
     qualification: 'BE',
     experience: '5-10',
     organisation: 'XYZ Company',
-    skill: 'React',
+    skill: 'react,devops',
+    notification: true,
+    cv: nil,
+    picture: nil,
+    certificate: nil,
+    job: 'Cook,Engineer,Receiptionist',
+    cvdownload: 'Connections',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  {
+    user_id: 6,
+    username: 'sritam9',
+    gmail: 'sritamd9@gmail.com',
+    link: nil,
+    qualification: 'BE',
+    experience: '5-10',
+    organisation: 'XYZ Company',
+    skill: 'ajile',
     notification: true,
     cv: nil,
     picture: nil,
@@ -65,34 +139,233 @@ user_data = [
     job: 'Cook',
     cvdownload: 'Connections',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  {
+    user_id: 7,
+    username: 'sritam9',
+    gmail: 'sritamd9@gmail.com',
+    link: nil,
+    qualification: 'BE',
+    experience: '5-10',
+    organisation: 'XYZ Company',
+    skill: 'rails',
+    notification: true,
+    cv: nil,
+    picture: nil,
+    certificate: nil,
+    job: 'Cook,Developer',
+    cvdownload: 'Connections',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  {
+    user_id: 8,
+    username: 'sritam9',
+    gmail: 'sritamd9@gmail.com',
+    link: nil,
+    qualification: 'BE',
+    experience: '5-10',
+    organisation: 'XYZ Company',
+    skill: 'c,rails',
+    notification: true,
+    cv: nil,
+    picture: nil,
+    certificate: nil,
+    job: 'Cook,,WebEngineer,Engineer',
+    cvdownload: 'Connections',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  {
+    user_id: 9,
+    username: 'sritam9',
+    gmail: 'sritamd9@gmail.com',
+    link: nil,
+    qualification: 'BE',
+    experience: '5-10',
+    organisation: 'XYZ Company',
+    skill: 'java',
+    notification: true,
+    cv: nil,
+    picture: nil,
+    certificate: nil,
+    job: 'Cook,Caretaker',
+    cvdownload: 'Connections',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  {
+    user_id: 10,
+    username: 'sritam9',
+    gmail: 'sritamd9@gmail.com',
+    link: nil,
+    qualification: 'BE',
+    experience: '5-10',
+    organisation: 'XYZ Company',
+    skill: 'python,c',
+    notification: true,
+    cv: nil,
+    picture: nil,
+    certificate: nil,
+    job: 'Cook,WebEngineer',
+    cvdownload: 'Connections',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
   }
+
 ]
 
 user_data.each do |data|
   UserAccount.create!(data)
 end
 
-user_ids = [4, 5]
 
-user_ids.each do |user_id|
-  user = User.find(user_id)
-  user.job_navigations.create(
-    jobtitle: 'Software Engineer',
-    jobdescription: 'We are seeking an experienced software engineer to join our team.',
-    vacancy: '11',
-    skill: 'Ruby on Rails, React, PostgreSQL',
-    jobrole: 'Developer',
-    jobsector: 'Corporate',
+
+job_data = [
+  {
+    user_id: 1,
+    jobtitle: "WebEngineer",
+    jobdescription: "We are seeking an experienced software engineer to join our team.",
+    vacancy: "2 positions",
+    skill: "Ruby on Rails, React, PostgreSQL",
+    jobrole: "Full-time",
+    jobsector: "Technology",
+    is_approved: false
+},
+{
+    user_id: 2,
+    jobtitle: "Developer",
+    jobdescription: "We are seeking an experienced software engineer to join our team.",
+    vacancy: "2",
+    skill: "Ruby on Rails, React, PostgreSQL",
+    jobrole: "Full-time",
+    jobsector: "Technology",
     is_approved: true
-  )
+},
+{
+    user_id: 3,
+    jobtitle: "Caretaker",
+    jobdescription: "We are seeking an experienced software engineer to join our team.",
+    vacancy: "5",
+    skill: "Ruby on Rails, React, PostgreSQL",
+    jobrole: "Full-time",
+    jobsector: "Technology",
+    is_approved: false
+},
+{
+    user_id: 4,
+    jobtitle: "Cook",
+    jobdescription: "We are seeking an experienced software engineer to join our team.",
+    vacancy: "18",
+    skill: "Ruby on Rails, React, PostgreSQL",
+    jobrole: "Full-time",
+    jobsector: "Technology",
+    is_approved: false
+},
+{
+    user_id: 5,
+    jobtitle: "Receiptionist",
+    jobdescription: "We are seeking an experienced software engineer to join our team.",
+    vacancy: "7",
+    skill: "Ruby on Rails, React, PostgreSQL",
+    jobrole: "Full-time",
+    jobsector: "Technology",
+    is_approved: false
+},
+{
+    user_id: 6,
+    jobtitle: "Caretaker",
+    jobdescription: "We are seeking an experienced software engineer to join our team.",
+    vacancy: "22",
+    skill: "Ruby on Rails, React, PostgreSQL",
+    jobrole: "Full-time",
+    jobsector: "Technology",
+    is_approved: false
+},
+{
+    user_id: 7,
+    jobtitle: "Driver",
+    jobdescription: "We are seeking an experienced software engineer to join our team.",
+    vacancy: "66",
+    skill: "react,ajile",
+    jobrole: "Full-time",
+    jobsector: "Technology",
+    is_approved: true
+},
+{
+    user_id: 8,
+    jobtitle: "Receiptionist",
+    jobdescription: "We are seeking an experienced software engineer to join our team.",
+    vacancy: "20",
+    skill: "java,c",
+    jobrole: "Full-time",
+    jobsector: "Technology",
+    is_approved: true
+},
+{
+    user_id: 9,
+    jobtitle: "Developer",
+    jobdescription: "We are seeking an experienced software engineer to join our team.",
+    vacancy: "56",
+    skill: "Ruby on Rails, React, Python",
+    jobrole: "Full-time",
+    jobsector: "Technology",
+    is_approved: true
+  },
+{
+    user_id: 10,
+    jobtitle: "Software Engineer",
+    jobdescription: "We are seeking an experienced software engineer to join our team.",
+    vacancy: "77",
+    skill: "Ruby on Rails, React, PostgreSQL",
+    jobrole: "Full-time",
+    jobsector: "Technology",
+    is_approved: false
+}
+]
+
+job_data.each do |data|
+  JobNavigation.create!(data)
 end
 
-# create a new job field record for the user
 job_field = JobField.create(jobsection: 'Finance')
 
 # create a new job position record for the user
 job_position = JobPosition.create(jobdesignation: 'CA')
 
-# db/seeds.rb
+users[1].send_follow_request_to(users[9])
+users[9].accept_follow_request_of(users[1])
+
+users[3].send_follow_request_to(users[2])
+users[2].accept_follow_request_of(users[3])
+
+users[8].send_follow_request_to(users[3])
+users[3].accept_follow_request_of(users[8])
+
+
+users[1].send_follow_request_to(users[4])
+users[4].accept_follow_request_of(users[1])
+
+users[1].send_follow_request_to(users[5])
+users[5].accept_follow_request_of(users[1])
+
+
+users[2].send_follow_request_to(users[6])
+users[6].accept_follow_request_of(users[2])
+
+
+users[3].send_follow_request_to(users[7])
+users[7].accept_follow_request_of(users[3])
+
+
+users[4].send_follow_request_to(users[8])
+users[8].accept_follow_request_of(users[4])
+
+
+users[5].send_follow_request_to(users[9])
+users[9].accept_follow_request_of(users[5])
+
+
+
+
+
+
+
 
 # generate 20 users
